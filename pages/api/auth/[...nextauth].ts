866933184669cf6b1f365807b7f8e44d97acacda
Arushi Somani <arushisomani@berkeley.edu>
2023-06-03 13:27:45 -0700

    cleanup

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,14 +1,19 @@
+// Disable all linting for this file because it's copied from the NextAuth docs
+
 import NextAuth, { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
-import FacebookProvider from "next-auth/providers/facebook"
-import GithubProvider from "next-auth/providers/github"
-import TwitterProvider from "next-auth/providers/twitter"
-import Auth0Provider from "next-auth/providers/auth0"
-
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
+
+if (!process.env.GOOGLE_ID || !process.env.GOOGLE_SECRET) {
+  throw new Error(
+    "Missing environment variables for Google OAuth. Check your .env file."
+  )
+}
+
 export const authOptions: NextAuthOptions = {
   // https://next-auth.js.org/configuration/providers/oauth
+
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
